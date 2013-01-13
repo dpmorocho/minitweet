b5aabdc473eea54dd926d7e9b2764c744064e109
Diego Morocho <dpmorocho@gmail.com>
2013-01-13 17:12:27 -0500

    Actualizacion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,7 @@ Minitweet::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  #cetificados
-  config.force ssl = true
+
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
